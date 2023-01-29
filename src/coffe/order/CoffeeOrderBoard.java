f26057fcf6d4944a619cbcfb7b3e54b362e5db70
Sytnyk Yurii <yoursit1980@gmail.com>
2023-01-29 22:31:35 +0200

    homework15

--- src/coffe/order/CoffeeOrderBoard.java
+++ src/coffe/order/CoffeeOrderBoard.java
@@ -1,2 +1,53 @@
-package coffe.order;public class CoffeeOrderBoard {
+package coffe.order;
+
+import java.util.*;
+
+public class CoffeeOrderBoard {
+
+    private final List<Order> orderList = new ArrayList<>();
+
+    public void add(Order order) {
+
+        if (orderList.contains(order) || order.getNumberOrder() == 0) {
+            throw new IllegalArgumentException("Wrong ! You entered an invalid numId.");
+        }
+        try {
+            orderList.add(order);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        Collections.sort(orderList);
+    }
+
+    public void deliver() {
+        orderList.remove(0);
+    }
+
+    public void deliver(Integer num) {
+        for (Order order : orderList) {
+            if (order.getNumberOrder() == num) {
+                orderList.remove(order);
+                break;
+            }
+        }
+    }
+
+    public void draw() {
+        String showTitle = "ID | Client\n";
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Order order : orderList) {
+            stringBuilder.append("\n").append(order);
+        }
+        System.out.println(showTitle + stringBuilder);
+    }
 }
+
+
+
+
+
+
+
+
+
+
--- src/coffe/order/Main.java
+++ src/coffe/order/Main.java
@@ -1,2 +1,19 @@
-package coffe.order;public class Main {
+package coffe.order;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        CoffeeOrderBoard coffeeOrderBoard = new CoffeeOrderBoard();
+        coffeeOrderBoard.add(new Order(1, "Bob"));
+        coffeeOrderBoard.add(new Order(3, "TOM"));
+        coffeeOrderBoard.add(new Order(4, "Mark"));
+        coffeeOrderBoard.add(new Order(6, "Evgeniy"));
+        coffeeOrderBoard.add(new Order(5, "Anatoliy"));
+        coffeeOrderBoard.add(new Order(2, "Egor"));
+
+        coffeeOrderBoard.deliver();
+        coffeeOrderBoard.deliver(6);
+        coffeeOrderBoard.draw();
+    }
 }
